93cd3c9c9c5f39083c988e1df6684edb36afc081
p.botov <p.botov@crpt.ru>
2021-04-21 19:19:39 +0300

    Changed travis

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Wow there!</p>
+        <p>What the fuck there!</p>
         <a
           className="App-link"
           href="https://reactjs.org"
